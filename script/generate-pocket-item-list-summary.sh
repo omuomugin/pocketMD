e0578ffda78f748ee40ba9086f519cd7b1d65a18
omuomugin <m@omuomugin.com>
2025-05-03 16:26:10 +0900

    feat: support for front matter

--- script/generate-pocket-item-list-summary.sh
+++ script/generate-pocket-item-list-summary.sh
@@ -7,6 +7,17 @@ OUTPUT_FILE="${OUTPUT_FAVORITE_ITEM_LIST_FILE_PATH}"
 
 CURRENT_DATE=$(date '+%Y/%m/%d')
 
+# clean up the output file
+echo "" > "$OUTPUT_FILE"
+
+# Check if FRONTMATTER is set
+if [ -n "${FRONT_MATTER}" ]; then
+  echo "---" > "$OUTPUT_FILE"
+  echo "${FRONT_MATTER}" >> "$OUTPUT_FILE"
+  echo "---" >> "$OUTPUT_FILE"
+  echo "" >> "$OUTPUT_FILE"
+fi
+
 echo "updated_at: $CURRENT_DATE" >> "$OUTPUT_FILE"
 echo "" >> "$OUTPUT_FILE"
 
